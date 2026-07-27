528a95fc3f4fd2d42a7029c57245a859624835af
Hn15 <azzamgantengs123@gmail.com>
2026-07-27 11:04:35 +0800

    a

---
+++ contoh1.java
@@ -0,0 +1,22 @@
+//buatlah kode yang outputnya
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+// dan seterusnya
+
+public class contoh1 {
+    public static void main(String[] args) {
+        int n = 67;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+

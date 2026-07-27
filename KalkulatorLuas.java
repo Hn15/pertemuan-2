ba4f9f385fa90bc7129a43c4159bf9f76fdee3b4
Hn15 <azzamgantengs123@gmail.com>
2026-07-27 10:07:43 +0800

    first commit

---
+++ KalkulatorLuas.java
@@ -0,0 +1,20 @@
+public class KalkulatorLuas {
+
+    public int hitungLuasPersegiPanjang(int panjang, int lebar) {
+        return panjang * lebar;
+    }
+
+    public static void main(String[] args) {
+        // Membuat objek dari class KalkulatorLuas
+        KalkulatorLuas kalkulator = new KalkulatorLuas();
+
+        int panjang = 10;
+        int lebar = 5;
+
+        int hasilLuas = kalkulator.hitungLuasPersegiPanjang(panjang, lebar);
+
+        System.out.println("Panjang: " + panjang);
+        System.out.println("Lebar: " + lebar);
+        System.out.println("Luas Persegi Panjang: " + hasilLuas);
+    }
+}
